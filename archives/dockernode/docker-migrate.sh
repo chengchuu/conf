8dea79dbf27db5a912f2cc7d3eaeeb6c29f92b36
Cheng <mazeyqian@gmail.com>
2026-01-10 15:13:34 +0800

    chore(arc): delete surplus files

--- archives/dockernode/docker-migrate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# bash ./docker-build.sh -r
-set -e
-echo "Migrate Configurations ..."
-# Migrate Dcockerfile & Conf
-\cp -f ./conf/config/dockernode/Dockerfile ./
-\cp -f ./conf/.dockerignore ./
-\cp ./server/config/node-feperf-monitor-server/config.prod.js ./node-feperf-monitor-server/config/config.prod.js;
