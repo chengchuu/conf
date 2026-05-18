95e020a3a446557c05f9f6504f8ab152b6bd196c
Cheng <mazeyqian@gmail.com>
2026-05-18 12:04:32 +0800

    feat(mazey): dockerfile, add proxy

--- config/dockernode/docker-entrypoint.sh
+++ config/dockernode/docker-entrypoint.sh
@@ -4,6 +4,7 @@ npm install -g pm2@2.10.3 --registry=https://registry.npmmirror.com
 # Rain
 cd /web/node-koa-rain
 npm ci
+# npm install one-time@0.0.4 gtts@0.2.1 exceljs@4.3.0 --registry=https://registry.npmmirror.com
 npm run start
 # Server
 cd /web/server
